f0c9472e0de610157fa87fc209e01c6bcbf747a2
Alexander Wolf <alex.v.wolf@gmail.com>
2020-03-07 15:01:29 +0700

    Added tool for download fresh pulsars catalog and extract the data for our JSON catalog of pulsars

---
+++ psrcat/make_catalog.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+catalog=psrcat_tar
+archive=psrcat_pkg.tar.gz
+
+echo "[Step 1] Downloading file...\n"
+curl -# --retry 3 --connect-timeout 720 -R -O https://www.atnf.csiro.au/research/pulsar/psrcat/downloads/psrcat_pkg.tar.gz
+sleep 5
+echo "[Step 2] Extract files...\n"
+tar -zxvf $archive
+echo "[Step 3] Parse catalog and generate JSON...\n"
+./psrcat2json.pl
+echo "[Step 4] Remove unused data...\n";
+rm -rf $catalog
+rm -f $archive
+echo "[Step 5] Validate catalog...\n";
+jsonlint-py3 -v ./pulsars.json
