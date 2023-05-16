140cc4de0eded2e0cb391e1cd78b94ba33da5565
Alexander V. Wolf <alex.v.wolf@gmail.com>
2023-05-16 20:33:38 +0700

    Added tool for prepare discovery circumstances of minor planets

---
+++ discovery-mp/all_process.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+url="https://minorplanetcenter.net/iau/lists/NumberedMPs.txt.gz"
+
+echo "[Step 1] Downloading Discovery Circumstances...\n"
+
+echo "Processing $url"
+curl -# --retry 3 --connect-timeout 720 -R -O $url
+sleep 5
+
+echo "[Step 2] Extract text...\n"
+gzip -d ./*.gz
+
+echo "[Step 3] Create Discovery Circumstances for Stellarium...\n"
+
+cp -f discovery_circumstances.fab discovery_circumstances.previous
+
+./generate_discovery_circumstances.pl
+
+gzip -nc discovery_circumstances.fab > discovery_circumstances.dat
+
+oldFileSize=$(stat -c%s "./discovery_circumstances.previous")
+newFileSize=$(stat -c%s "./discovery_circumstances.fab")
+
+echo "[Step 3] Clean up...\n"
+
+rm ./*.txt ./*.previous
+
+if [ $oldFileSize != $newFileSize ]
+then
+    echo "Discovery Circumstances is changed!\n"
+fi
---
+++ discovery-mp/generate_discovery_circumstances.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+#
+# This is tool to convert discovery circumstances of minor planets from MPC format 
+# into Stellarium's format
+#
+# Copyright (c) 2023 Alexander Wolf
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+open(ORIG, "<./NumberedMPs.txt");
+@original = <ORIG>;
+close ORIG;
+
+$delimiter = "\t";
+
+open(FAB, ">./discovery_circumstances.fab");
+for($i=0; $i<scalar(@original); $i++)
+{
+    $text       = $original[$i];
+
+    if ($text =~ m/\((\d+)\)/gi) { $number = $1; }
+    $date       = substr($text, 41, 10);
+    $date       =~ s/\s/-/gi;
+    $discoverer = substr($text, 78);
+    if ($discoverer =~ m/,/gi) 
+    {
+        @fio = split(", ", $discoverer);
+        @names = ();
+        for($j=0; $j<scalar(@fio); $j=$j+2) 
+        {
+            $initials = $fio[$j+1];
+            $initials =~ s/\s+//gi;
+            $initials =~ s/\r//gi;
+            $initials =~ s/\n//gi;
+            push @names, $initials." ".$fio[$j];
+        }
+        if (scalar(@names)>3) {
+    	    $discoverer = $names[0]." et.al.";
+        } else {
+            $discoverer = join(", ", @names);
+        }
+        $discoverer .= "\n";
+    }
+
+    # special case: Spacewatch at Kitt Peak
+    if ($discoverer =~ m/Spacewatch/) { $discoverer = "Spacewatch at Kitt Peak\n"; }
+
+    print FAB $number.$delimiter.$date.$delimiter.$discoverer;
+}
+
+close FAB;
+
