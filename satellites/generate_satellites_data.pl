b8e1d704f3e6dd16651c9903a0195fd9ac8ca248
Alexander V. Wolf <alex.v.wolf@gmail.com>
2024-05-05 10:54:45 +0700

    Added 2 sources for standard magnitudes and RCS data for our catalog

--- satellites/generate_satellites_data.pl
+++ satellites/generate_satellites_data.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 #
 # This is tool to extract standard magnitudes and radar cross-section (RCS) data from the various
-# files, which are distributed on Make McCants' web page https://mmccants.org
+# files, which are distributed on Make McCants' web page (https://mmccants.org), MMT-9 observatory 
+# (http://mmt.favor2.info/satellites), and CelesTrack web page (https://celestrak.org)
 #
 # Copyright (c) 2024 Alexander Wolf
 #
@@ -29,13 +30,15 @@ use strict;
 # data
 my $stdmagFile	= "./qs.mag";
 my $RCSFile	= "./rcs";
+my $MMTFile	= "./download";
+my $SCFile	= "./satcat.txt";
 # templates
 my $headerFile	= "./header.tmpl";
 # results
 my $satDFile	= "./satellites.fab";
 
 my %satellites	= ();
-my $delimiter	= " ";
+my $delimiter	= "\t";
 
 my @header;
 open(TMPL, "<$headerFile");
@@ -88,6 +91,58 @@ for(my $j=0; $j<scalar(@rcsdata); $j++)
     }
 }
 
+# read standard magnitudes from MMT-9
+my @mmtdata;
+open(MMT, "<$MMTFile");
+@mmtdata = <MMT>;
+close MMT;
+
+for(my $i=0; $i<scalar(@mmtdata); $i++)
+{
+    my $data  = $mmtdata[$i];
+    if (substr($data, 0, 1) eq '#') 
+    { 
+	next;
+    }
+    if ($data =~ m/^(NORAD|McCants)\s(\d+)\s\"(.+)\"\s\w+\s\d\s\d\s([\.\d]+)\s/)
+    {
+	my $NORAD = $2;
+	my $mag   = $4;
+	if (not exists $satellites{$NORAD})
+	{
+	    $satellites{$NORAD} = $mag."::";
+	}
+    }
+}
+
+# read radar cross section (RCS) from CelesTrack
+my @scdata;
+open(SCF, "<$SCFile");
+@scdata = <SCF>;
+close SCF;
+
+for(my $i=0; $i<scalar(@scdata); $i++)
+#for(my $i=0; $i<25; $i++)
+{
+    my $data  = $scdata[$i];
+    my $NORAD = substr($data, 13, 5);
+    my $rcsd  = substr($data, 119, 8);
+    $NORAD    = $NORAD+0;
+    $rcsd     =~ s/\s+//gi;
+    $rcsd     =~ s/N\/A/-/gi;
+    if ($rcsd ne '-' && $rcsd ne '')
+    {
+	if (exists $satellites{$NORAD})
+	{
+	    my @val = split("::", $satellites{$NORAD});
+	    $satellites{$NORAD} = $val[0]."::".$rcsd;
+	} else {
+	    $satellites{$NORAD} = "::".$rcsd;
+	}
+    }
+}
+
+# write satellites.fab file (joint data)
 open(FAB, ">$satDFile");
 for(my $k=0; $k<scalar(@header); $k++) 
 {
@@ -97,10 +152,10 @@ print FAB "\n";
 
 foreach my $sat (sort { $a <=> $b } keys %satellites) 
 {
-    my $noradID = sprintf("%5d", $sat);
+    my $noradID = sprintf("%6d", $sat);
     my @dat = split("::", $satellites{$sat});
-    my $magID   = ($dat[0] eq '') ? sprintf "%6s" , $dat[0] : sprintf "%+.3f", $dat[0];
-    my $rcsID   = ($dat[1] eq '') ? sprintf "%7s" , $dat[1] : sprintf "%7s" , (sprintf "%.3f", $dat[1]);
+    my $magID   = ($dat[0] eq '') ? sprintf "%8s" , $dat[0] : sprintf "%8s" , (sprintf "%.4f", $dat[0]);
+    my $rcsID   = ($dat[1] eq '') ? sprintf "%8s" , $dat[1] : sprintf "%8s" , (sprintf "%.4f", $dat[1]);
     if ($dat[0] ne '' || $dat[1] ne '')
     {
 	print FAB $noradID.$delimiter.$magID.$delimiter.$rcsID."\n";
